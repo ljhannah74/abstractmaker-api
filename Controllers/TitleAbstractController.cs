90b939049a0d5fd1a27b395491924b72ab3fada3
Lewis Hannah <70963125+ljhannah74@users.noreply.github.com>
2024-10-03 21:18:43 -0400

    Update TitleAbstractController.cs

--- Controllers/TitleAbstractController.cs
+++ Controllers/TitleAbstractController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace abstractmaker_api.Controllers;
 
-[Route("api/[controller]")]
+[Route("[controller]")]
 [ApiController]
 public class TitleAbstractController : ControllerBase
 {
